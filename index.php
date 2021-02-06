37f88807e75eb219ec34306c1b5813e6d950ee83
Anglebert Masunda <37086822+Eng-Masunda@users.noreply.github.com>
2021-02-06 10:43:27 +0200

    Removed email password

--- index.php
+++ index.php
@@ -46,8 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 			$mail->isSMTP();                                            // Send using SMTP
 			$mail->Host       = "smtp.gmail.com";                    // Set the SMTP server to send through
 			$mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-			$mail->Username   = 'iengineerwebsolutions@gmail.com';                     // SMTP username
-			$mail->Password   = 'bestDevInZim';                               // SMTP password
+			$mail->Username   = 'user-email@gmail.com';                     // SMTP username
+			$mail->Password   = 'user-email-password';                               // SMTP password
 			$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 			$mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
